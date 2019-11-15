31220c92cae600afb2a2a6da2e518197276550dc
LeeHyungJoo <>
2019-11-15 15:55:40 +0200

    no message

--- Node1.py
+++ Node1.py
@@ -1,46 +1,62 @@
 import socket
-from _thread import *
+import _thread
+import time
 
 
 def deal_with_customer(client_socket, addr):
 
     print('Connection created with :', addr[0], ':', addr[1])
     print('Waiting request from the client.')
-    data = client_socket.recv(1024)
+
+    price = client_socket.recv(1024)
+    info_customer = data.decode().split()
+    
 
 
     if (data.decode() == "Payment Request"):
-        start_new_thread(call_node1())
-        start_new_thread(call_node2())
+        _thread.start_new_thread(call_node2())
+        _thread.start_new_thread(call_node3())
+
+        ##something for synchronization
 
 
+        if (OK):
+            client_socket.send(something)
+        elif (not OK):
+            client_socket.send(something)
 
 
-def call_node1():
-    sock_for_N2.send("something")
-    sock_for_N2.recv(1024)
 
 
 def call_node2():
-    sock_for_N3.send("something")
+    sock_for_N2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_for_N2.connect(("localhost", 8001))
+
+    sock_for_N2.send("something_to_Node2")
+    sock_for_N2.recv(1024)
+
+
+
+def call_node3():
+    sock_for_N3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_for_N3.connect(("localhost", 8002))
+
+    sock_for_N3.send("something_to_Node3")
     sock_for_N3.recv(1024)
+
+
+
+
 
 
 socket_for_clients = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket_for_clients.bind(("localhost", 8000))
 socket_for_clients.listen()
 
-sock_for_N2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock_for_N2.connect(("localhost", 8001))
-
-sock_for_N3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock_for_N3.connect(("localhost", 8002))
-
 
 while True:
     print('Waiting connction from a client.')
     client_socket, addr = socket_for_clients.accept()
-
-    start_new_thread(deal_with_customer(client_socket, addr))
+    _thread.start_new_thread(deal_with_customer, (client_socket, addr))
 
 
