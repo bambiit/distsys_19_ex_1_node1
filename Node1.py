a2e8196386eaf599833b64abe7f54456f63da710
LeeHyungJoo <>
2019-11-17 18:11:58 +0200

    Port change. The way of creating thread changed

--- Node1.py
+++ Node1.py
@@ -1,6 +1,8 @@
 import socket
-import _thread
-import time
+import threading
+from multiprocessing.pool import ThreadPool
+
+
 
 
 def deal_with_customer(client_socket, addr):
@@ -8,41 +10,51 @@ def deal_with_customer(client_socket, addr):
     print('Connection created with :', addr[0], ':', addr[1])
     print('Waiting request from the client.')
 
-    price = client_socket.recv(1024)
-    info_customer = data.decode().split()
-    
+    price = client_socket.recv(1024).decode()
+
+    info_customer = client_socket.recv(1024).decode()
+    name = info_customer.split(", ")[0]
+    bank_account = info_customer.split(", ")[1]
+
+    pool = ThreadPool(processes=1)
+
+    if (len(str(bank_account)) == 18):
+        async_thread1_result = pool.apply_async(call_node2, (name, bank_account))
+        async_thread2_result = pool.apply_async(call_node3, (name, bank_account))
+
+        print("Node1 called other two nodes.")
+
+        if(async_thread1_result.get() == 'OK'):
+            if(async_thread1_result.get() == 'OK'):
+                client_socket.send("OK".encode())
+            else:
+                client_socket.send(async_thread2_result.get())
+        else:
+            client_socket.send(async_thread2_result.get())
+    else:
+        print("Wrong bank account number")
+        client_socket.send("Wrong bank account number".encode())
 
 
-    if (data.decode() == "Payment Request"):
-        _thread.start_new_thread(call_node2())
-        _thread.start_new_thread(call_node3())
 
-        ##something for synchronization
-
-
-        if (OK):
-            client_socket.send(something)
-        elif (not OK):
-            client_socket.send(something)
-
-
-
-
-def call_node2():
+def call_node2(name, bankaccount):
     sock_for_N2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock_for_N2.connect(("localhost", 8001))
+    sock_for_N2.connect(("localhost", 8002))
+    print("Node 2 connected")
 
-    sock_for_N2.send("something_to_Node2")
-    sock_for_N2.recv(1024)
+    sock_for_N2.send()
+    return sock_for_N2.recv(1024).decode()
 
 
 
-def call_node3():
+
+def call_node3(name, bankaccount):
     sock_for_N3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock_for_N3.connect(("localhost", 8002))
+    sock_for_N3.connect(("localhost", 8003))
+    print("Node 3 connected")
 
-    sock_for_N3.send("something_to_Node3")
-    sock_for_N3.recv(1024)
+    sock_for_N3.send(bytes(bankaccount, "utf-8"))
+    return sock_for_N3.recv(1024).decode()
 
 
 
@@ -50,13 +62,14 @@ def call_node3():
 
 
 socket_for_clients = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket_for_clients.bind(("localhost", 8000))
+socket_for_clients.bind(("localhost", 8001))
 socket_for_clients.listen()
 
 
 while True:
     print('Waiting connction from a client.')
     client_socket, addr = socket_for_clients.accept()
-    _thread.start_new_thread(deal_with_customer, (client_socket, addr))
+    threading.Thread(target=deal_with_customer, args=(client_socket, addr)).start()
+
 
 
